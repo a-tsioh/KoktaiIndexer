d99f0f4333d3636686a83f8ee06cfad8b1f94eca
a-tsioh <magistry@limsi.fr>
2018-07-16 20:09:11 +0200

    render ruby (wip)

--- client/src/main/scala/fr/magistry/koktai/views/WordPage.scala
+++ client/src/main/scala/fr/magistry/koktai/views/WordPage.scala
@@ -5,6 +5,18 @@ import scalatags.JsDom.all._
 
 class WordPage(dispatch: Dispatcher, w: api.Word)  {
 
+  lazy val definition = {
+    div(w.definition map {
+      case api.SimpleText(t) => raw(t)
+      case api.Ruby(r,t,img) => div(
+        cls:="ruby_chr",
+        div(cls:="ruby_syl", r)
+      )
+      case api.KoktaiCJK(cjk,img) => raw(cjk)
+    })
+
+  }
+
   protected val content =
     div(
       cls:="ui card",
@@ -13,7 +25,7 @@ class WordPage(dispatch: Dispatcher, w: api.Word)  {
         div(cls:="header",w.orth),
         div(cls:="meta",w.pron),
         div(cls:="meta",w.pos),
-        div(cls:="description",w.definition)
+        div(cls:="description", definition)
       )
     ).render
 
--- server/app/models/ApiService.scala
+++ server/app/models/ApiService.scala
@@ -1,19 +1,35 @@
 package models
 
+import xml.XML
+
 object ApiService extends api.SharedApi {
 
   val esClient = new ElasticSerchClient()
+
+  private def renderDefinition(w: datatypes.Word) = {
+    val x = XML.loadString(w.xmlSource)
+    val definition = (x \\ "def").head.child map {
+      case x if x.label == "#PCDATA" => api.SimpleText(x.text)
+      case x if x.label == "g" =>
+        if(x.attribute("ref").map(_.text.startsWith("ruby")).getOrElse(false)) {
+          api.Ruby(x.text,"Y","TODO")
+        }
+        else api.KoktaiCJK(x.text,"Y")
+    }
+    definition
+  }
 
 
   override def test(): api.Chapter = api.Chapter("coucou",Nil,Nil,"comment")
 
   override def simpleWordSearch(query: String): Seq[api.Word] = {
-    esClient.searchWords(query).map {w => api.Word(w.form,w.orth, w.pron, w.pos, w.definition, w.xmlSource)}
+    esClient.searchWords(query).map {w => api.Word(w.form,w.orth, w.pron, w.pos, renderDefinition(w), w.xmlSource)}
   }
 
   override def combinedSearch(query: String): Seq[api.AnyElem] = {
     esClient.searchCombined(query).map {
-      case w: datatypes.Word => api.AnyElem(Some(api.Word(w.form,w.orth, w.pron, w.pos, w.definition, w.xmlSource)),None)
+      case w: datatypes.Word =>
+        api.AnyElem(Some(api.Word(w.form,w.orth, w.pron, w.pos, renderDefinition(w), w.xmlSource)),None)
       case datatypes.Sinogram(orth, pron, comment, guoyin, taikam, phooban, words, xmlSource) =>
         api.AnyElem(None, Some(api.Sinogram(orth, pron, comment, guoyin, taikam, phooban, words, xmlSource)))
     }
